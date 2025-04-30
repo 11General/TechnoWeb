b92bfd2ea9784717641704e20098b35c1b3981e6
11General <mbaye.gaye.663@gmail.com>
2025-04-30 14:33:01 +0000

    Delete studentmanager directory

--- studentmanager/src/main/java/techno/web/studentmanager/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package techno.web.studentmanager.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
