038ded6557464b16ecb1c2f4dcf655b898be511f
Mbaye <mbaye.gaye.663gmail.com>
2025-04-30 15:22:40 +0000

    init

---
+++ studentManagerSpringBoot/src/main/java/techno/web/studentmanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package techno.web.studentmanager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
